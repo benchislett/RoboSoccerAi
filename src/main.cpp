147d0b283f57fd7a6a02b744a8cee5ae9e4d0446
benchislett <chislett.ben@gmail.com>
2021-12-09 10:05:50 -0500

    Working debug draw, fix wheel pos and collisions

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,97 @@
 
 using std::numbers::pi;
 
-constexpr int width = 1024;
+constexpr int width = 512;
+constexpr int height = 512;
 
-constexpr int height = 768;
+constexpr int world_size = 128;
+
+constexpr float x_scale = width / world_size;
+constexpr float y_scale = height / world_size;
 
 constexpr int bot_width = 70;
 constexpr int bot_height = 30;
 
-constexpr float bot_width_f = (float)bot_width / (float)width;
-constexpr float bot_height_f = (float)bot_height / (float)height;
+constexpr float bot_width_f =  (float)bot_width / (float)width * x_scale;
+constexpr float bot_height_f = (float)bot_height / (float)height * y_scale;
+
+sf::Color b2ColorToSfColor(b2Color color, int alpha = 255) {
+  return sf::Color((sf::Uint8)(color.r*255), (sf::Uint8)(color.g*255), (sf::Uint8)(color.b*255), (sf::Uint8) alpha);
+}
+
+class SfDebugDraw : public b2Draw {
+  sf::RenderWindow* window;
+
+public:
+  SfDebugDraw(sf::RenderWindow* win) : window(win) {}
+  ~SfDebugDraw() {}
+
+  void DrawPolygon(const b2Vec2* vertices, int32 vertex_count, const b2Color& color, bool fill) {
+    sf::ConvexShape poly;
+    poly.setPointCount(vertex_count);
+    for (int i = 0; i < vertex_count; i++) {
+      b2Vec2 vertex = vertices[i];
+      poly.setPoint(i, sf::Vector2f(vertex.x*world_size, vertex.y*world_size));
+    }
+    poly.setOutlineColor(b2ColorToSfColor(color, 50));
+    if (fill) {
+      poly.setFillColor(b2ColorToSfColor(color));
+    } else {
+      poly.setFillColor(sf::Color::Transparent);
+    }
+    window->draw(poly);
+  }
+
+  void DrawPolygon(const b2Vec2* vertices, int32 vertex_count, const b2Color& color) {
+    DrawPolygon(vertices, vertex_count, color, false);
+  }
+
+  void DrawSolidPolygon(const b2Vec2* vertices, int32 vertex_count, const b2Color& color) {
+    DrawPolygon(vertices, vertex_count, color, true);
+  }
+
+  void DrawCircle(const b2Vec2& center, float radius, const b2Color& color, bool fill) {
+    const float r = radius * world_size;
+    sf::CircleShape c(r);
+    c.setOrigin(r, r);
+    c.setPosition(center.x*world_size,center.y*world_size);
+    c.setOutlineColor(b2ColorToSfColor(color, 50));
+    if (fill) {
+      c.setFillColor(b2ColorToSfColor(color));
+    } else {
+      c.setFillColor(sf::Color::Transparent);
+    }
+    window->draw(c);
+  }
+
+  void DrawCircle(const b2Vec2& center, float radius, const b2Color& color) {
+    DrawCircle(center, radius, color, false);
+  }
+
+  void DrawSolidCircle(const b2Vec2& center, float radius, const b2Vec2& axis, const b2Color& color) {
+    DrawCircle(center, radius, color, true);
+  }
+
+  void DrawPoint(const b2Vec2& p, float size, const b2Color& color) {
+    sf::Vertex pt(sf::Vector2f(p.x*world_size, p.y*world_size), b2ColorToSfColor(color));
+    window->draw(&pt, 1, sf::Points);
+  }
+
+  void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) {
+    sf::Vertex p[2];
+    p[0] = sf::Vertex(sf::Vector2f(p1.x*world_size,p1.y*world_size), b2ColorToSfColor(color));
+    p[1] = sf::Vertex(sf::Vector2f(p2.x*world_size,p2.y*world_size), b2ColorToSfColor(color));
+    window->draw(p, 2, sf::Lines);
+  }
+
+  void DrawTransform(const b2Transform& xf) {
+    const float line_length = 0.05; // box2d units
+
+    b2Vec2 p1 = xf.p;
+    DrawSegment(p1, p1 + (line_length * xf.q.GetXAxis()), b2Color(1.f, 0.f, 0.f));
+    DrawSegment(p1, p1 - (line_length * xf.q.GetYAxis()), b2Color(0.f, 1.f, 0.f));
+  }
+};
 
 float clamp(float x, float lo, float hi) {
   if (x < lo) return lo;
@@ -23,24 +105,16 @@ float clamp(float x, float lo, float hi) {
   return x;
 }
 
-float angle(b2Body* body) {
-  float raw_rot = body->GetAngle();
-  float deg = -raw_rot;
-  while (deg < 0) deg += 360;
-  while (deg > 180) deg -= 360;
-  return deg * pi / 180.f;
-}
-
 b2Vec2 left_wheel(b2Body* body) {
   b2Vec2 pos = body->GetPosition();
-  float rot = angle(body);
+  float rot = -body->GetAngle();
   float left_rot = rot + (pi/2.f);
   return b2Vec2(pos.x + cosf(left_rot) * bot_height_f / 2.f, pos.y - sinf(left_rot) * bot_height_f / 2.f);
 }
 
 b2Vec2 right_wheel(b2Body* body) {
   b2Vec2 pos = body->GetPosition();
-  float rot = angle(body);
+  float rot = -body->GetAngle();
   float right_rot = rot - (pi/2.f);
   return b2Vec2(pos.x + cosf(right_rot) * bot_height_f / 2.f, pos.y - sinf(right_rot) * bot_height_f / 2.f);
 }
@@ -49,33 +123,15 @@ void Drive(b2Body* body, float left, float right) {
   left = clamp(left, -1, 1) / 100.0;
   right = clamp(right, -1, 1) / 100.0;
 
-  float rot = angle(body);
+  float rot = -body->GetAngle();
 
   b2Vec2 left_force(cosf(rot) * left, sinf(rot) * -left);
   b2Vec2 right_force(cosf(rot) * right, sinf(rot) * -right);
 
-  // printf("Rotation %f, Left wheel (%f, %f), Right wheel (%f, %f)\n", rot, left_wheel(body).x, left_wheel(body).y, right_wheel(body).x, right_wheel(body).y);
-  // printf("Position (%f, %f) applying force (%f, %f)\n", body->GetPosition().x, body->GetPosition().y, left_force.x, left_force.y);
+  left_force.x *= x_scale; right_force.x *= x_scale;
+  left_force.y *= y_scale; right_force.y *= y_scale;
   body->ApplyForce(left_force, left_wheel(body), true);
   body->ApplyForce(right_force, right_wheel(body), true);
-}
-
-void draw_wheels(b2Body* body, sf::RenderWindow& window) {
-  const float r = 1;
-  auto left = left_wheel(body);
-  auto right = right_wheel(body);
-
-  sf::CircleShape left_c(r);
-  sf::CircleShape right_c(r);
-  left_c.setOrigin(sf::Vector2f(r, r));
-  left_c.setPosition(sf::Vector2f(left.x*width, left.y*height));
-  left_c.setFillColor(sf::Color::Green);
-  right_c.setOrigin(sf::Vector2f(r, r));
-  right_c.setPosition(sf::Vector2f(right.x*width, right.y*height));
-  right_c.setFillColor(sf::Color::Green);
-
-  window.draw(left_c);
-  window.draw(right_c);
 }
 
 int main() {
@@ -85,11 +141,11 @@ int main() {
 
   b2BodyDef self_body_def;
   self_body_def.type = b2_dynamicBody;
-  self_body_def.position.Set(bot_width_f/2, 0.5f);
+  self_body_def.position.Set(bot_width_f/2.f, y_scale/2.f);
 
   b2BodyDef opp_body_def;
   opp_body_def.type = b2_dynamicBody;
-  opp_body_def.position.Set(1.f-bot_width_f/2, 0.5f);
+  opp_body_def.position.Set(x_scale-bot_width_f/2.f, y_scale/2.f);
 
   b2Body* self_body = world.CreateBody(&self_body_def);
   b2Body* opp_body = world.CreateBody(&opp_body_def);
@@ -110,14 +166,14 @@ int main() {
   const int velocityIterations = 6;
   const int positionIterations = 2;
 
-  const b2Vec2 right_force(1.f, 0.f);
-  const b2Vec2 left_force(-1.f, 0.f);
+  const b2Vec2 right_force(x_scale*y_scale, 0.f);
+  const b2Vec2 left_force(-x_scale*y_scale, 0.f);
 
   sf::RenderWindow window(sf::VideoMode(width, height), "RoboAI Window", sf::Style::Titlebar);
-  sf::RectangleShape self_rect(sf::Vector2f(bot_width, bot_height));
-  sf::RectangleShape opp_rect(sf::Vector2f(bot_width, bot_height));
-  self_rect.setFillColor(sf::Color(0, 0, 255));
-  opp_rect.setFillColor(sf::Color(255, 0, 0));
+
+  SfDebugDraw debugDraw(&window);
+  world.SetDebugDraw(&debugDraw);
+  debugDraw.SetFlags(0x00ff);
 
   window.setFramerateLimit(60);
 
@@ -141,26 +197,11 @@ int main() {
       window.close();
     }
 
-
     world.Step(timeStep, velocityIterations, positionIterations);
-    b2Vec2 self_position = self_body->GetPosition();
-    b2Vec2 opp_position = opp_body->GetPosition();
 
-    double dx=(self_position.x-opp_position.x);dx*=dx;
-    double dy=(self_position.y-opp_position.y);dy*=dy;
-    printf("Us: (%f, %f), Them: (%f, %f), Width: %f, Dist: %f\n", self_position.x, self_position.y, opp_position.x, opp_position.y, bot_width_f, sqrtf(dx+dy));
-
-    self_rect.setOrigin(self_rect.getSize().x / 2, self_rect.getSize().y / 2);
-    opp_rect.setOrigin(opp_rect.getSize().x / 2, opp_rect.getSize().y / 2);
-
-    self_rect.setPosition(self_position.x*width, self_position.y*height);
-    self_rect.setRotation(self_body->GetAngle());
-    opp_rect.setPosition(opp_position.x*width, opp_position.y*height);
-    opp_rect.setRotation(opp_body->GetAngle());
-
-    draw_wheels(self_body, window);
-    window.draw(self_rect);
-    window.draw(opp_rect);
+    world.DebugDraw();
+    debugDraw.DrawCircle(left_wheel(self_body), 0.005, b2Color(0,0,1), true);
+    debugDraw.DrawCircle(right_wheel(self_body), 0.005, b2Color(0,0,1), true);
     window.display();
     i++;
   }
