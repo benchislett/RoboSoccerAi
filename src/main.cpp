e64c9ad484c99b09d1590aeb03a0382cc6095c2a
benchislett <chislett.ben@gmail.com>
2021-12-09 19:17:17 -0500

    Box2d refactor

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,31 @@
 
 #include <cstdio>
 #include <cmath>
+#include <memory>
 #include <numbers>
 
+using std::make_unique;
+using std::unique_ptr;
 using std::numbers::pi;
 
-constexpr int width = 1024;
-constexpr int height = 768;
+// dimensions in pixels
+constexpr float width = 1024;
+constexpr float height = 768;
 
-constexpr float wheel_power = 0.01f;
+// pixels per box2d meter
+constexpr float length = 1024;
 
-constexpr int bot_width = 70;
-constexpr int bot_height = 30;
+constexpr float wheel_power = 20;
 
-constexpr float bot_width_f =  (float)bot_width / (float)width;
-constexpr float bot_height_f = (float)bot_height / (float)height;
+// dimension in pixels
+constexpr float bot_width = 70;
+constexpr float bot_height = 30;
+constexpr float ball_radius = 10;
+constexpr float wall_thickness = 5;
+
+// dimension in box2d
+constexpr float bot_width_f = bot_width / length;
+constexpr float bot_height_f = bot_height / length;
 
 sf::Color b2ColorToSfColor(b2Color color, int alpha = 255) {
   return sf::Color((sf::Uint8)(color.r*255), (sf::Uint8)(color.g*255), (sf::Uint8)(color.b*255), (sf::Uint8) alpha);
@@ -34,7 +45,7 @@ public:
     poly.setPointCount(vertex_count);
     for (int i = 0; i < vertex_count; i++) {
       b2Vec2 vertex = vertices[i];
-      poly.setPoint(i, sf::Vector2f(vertex.x*width, vertex.y*height));
+      poly.setPoint(i, sf::Vector2f(vertex.x*length, vertex.y*length));
     }
     poly.setOutlineColor(b2ColorToSfColor(color, 50));
     if (fill) {
@@ -54,10 +65,10 @@ public:
   }
 
   void DrawCircle(const b2Vec2& center, float radius, const b2Color& color, bool fill) {
-    const float r = radius * fmin(width, height);
+    const float r = radius * length;
     sf::CircleShape c(r);
     c.setOrigin(r, r);
-    c.setPosition(center.x*width,center.y*height);
+    c.setPosition(center.x*length,center.y*length);
     c.setOutlineColor(b2ColorToSfColor(color, 50));
     if (fill) {
       c.setFillColor(b2ColorToSfColor(color));
@@ -76,14 +87,14 @@ public:
   }
 
   void DrawPoint(const b2Vec2& p, float size, const b2Color& color) {
-    sf::Vertex pt(sf::Vector2f(p.x*width, p.y*height), b2ColorToSfColor(color));
+    sf::Vertex pt(sf::Vector2f(p.x*length, p.y*length), b2ColorToSfColor(color));
     window->draw(&pt, 1, sf::Points);
   }
 
   void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) {
     sf::Vertex p[2];
-    p[0] = sf::Vertex(sf::Vector2f(p1.x*width,p1.y*height), b2ColorToSfColor(color));
-    p[1] = sf::Vertex(sf::Vector2f(p2.x*width,p2.y*height), b2ColorToSfColor(color));
+    p[0] = sf::Vertex(sf::Vector2f(p1.x*length,p1.y*length), b2ColorToSfColor(color));
+    p[1] = sf::Vertex(sf::Vector2f(p2.x*length,p2.y*length), b2ColorToSfColor(color));
     window->draw(p, 2, sf::Lines);
   }
 
@@ -117,7 +128,7 @@ b2Vec2 right_wheel(b2Body* body) {
 }
 
 void Drive(b2Body* body, float left, float right) {
-  left = clamp(left, -1, 1) / 100.0;
+  left = clamp(left, -1, 1) / 100.0; // TODO: magic constant
   right = clamp(right, -1, 1) / 100.0;
 
   float rot = -body->GetAngle();
@@ -129,100 +140,124 @@ void Drive(b2Body* body, float left, float right) {
   body->ApplyForce(right_force, right_wheel(body), true);
 }
 
+struct Ball {
+  unique_ptr<b2CircleShape> shape;
+  unique_ptr<b2FixtureDef> fixture;
+  unique_ptr<b2BodyDef> body_def;
+  b2Body* body;
+
+  Ball(b2World& world, int spawn_x = width/2, int spawn_y = height/2) {
+    shape = make_unique<b2CircleShape>();
+    shape->m_radius = ball_radius / length;
+
+    fixture = make_unique<b2FixtureDef>();
+    fixture->shape = shape.get();
+    fixture->density = 1.f;
+    fixture->friction = 1.f;
+
+    body_def = make_unique<b2BodyDef>();
+    body_def->position.Set((float)spawn_x / length, (float)spawn_y / length);
+    body_def->type = b2_dynamicBody;
+    body_def->linearDamping = 1.f;
+    body_def->angularDamping = 2.f;
+  
+    body = world.CreateBody(body_def.get());
+    body->CreateFixture(fixture.get());
+  }
+
+  void reset() {
+    body->SetTransform(b2Vec2(width/length/2.f, height/length/2.f), 0.f);
+  }
+};
+
+struct Bot {
+  unique_ptr<b2PolygonShape> shape;
+  unique_ptr<b2FixtureDef> fixture;
+  unique_ptr<b2BodyDef> body_def;
+  b2Body* body;
+
+  Bot(b2World& world, int spawn_x, int spawn_y) {
+    shape = make_unique<b2PolygonShape>();
+    shape->SetAsBox(bot_width_f/2.f, bot_height_f/2.f);
+
+    fixture = make_unique<b2FixtureDef>();
+    fixture->shape = shape.get();
+    fixture->density = 10.f;
+    fixture->friction = 1.f;
+
+    body_def = make_unique<b2BodyDef>();
+    body_def->position.Set((float)spawn_x / length, (float)spawn_y / length);
+    body_def->type = b2_dynamicBody;
+    body_def->linearDamping = 3.f;
+    body_def->angularDamping = 3.f;
+  
+    body = world.CreateBody(body_def.get());
+    body->CreateFixture(fixture.get());
+  }
+};
+
+struct HorizontalWall {
+  unique_ptr<b2PolygonShape> shape;
+  unique_ptr<b2FixtureDef> fixture;
+  unique_ptr<b2BodyDef> body_def;
+  b2Body* body;
+
+  HorizontalWall(b2World& world, int spawn_x, int spawn_y, int w) {
+    shape = make_unique<b2PolygonShape>();
+    shape->SetAsBox(w/length/2.f, wall_thickness/length/2.f);
+
+    fixture = make_unique<b2FixtureDef>();
+    fixture->shape = shape.get();
+
+    body_def = make_unique<b2BodyDef>();
+    body_def->position.Set((float)spawn_x / length, (float)spawn_y / length);
+  
+    body = world.CreateBody(body_def.get());
+    body->CreateFixture(fixture.get());
+  }
+};
+
+struct VerticalWall {
+  unique_ptr<b2PolygonShape> shape;
+  unique_ptr<b2FixtureDef> fixture;
+  unique_ptr<b2BodyDef> body_def;
+  b2Body* body;
+
+  VerticalWall(b2World& world, int spawn_x, int spawn_y, int h) {
+    shape = make_unique<b2PolygonShape>();
+    shape->SetAsBox(wall_thickness/length/2.f, h/length/2.f);
+
+    fixture = make_unique<b2FixtureDef>();
+    fixture->shape = shape.get();
+
+    body_def = make_unique<b2BodyDef>();
+    body_def->position.Set((float)spawn_x / length, (float)spawn_y / length);
+  
+    body = world.CreateBody(body_def.get());
+    body->CreateFixture(fixture.get());
+  }
+};
+
 int main() {
 
   b2Vec2 gravity(0.f, 0.f);
   b2World world(gravity);
 
-  // WALLS
+  HorizontalWall top_wall(world, width/2, wall_thickness, width);
+  HorizontalWall bot_wall(world, width/2, height-wall_thickness, width);
 
-  b2PolygonShape horizontal_wall_shape;
-  horizontal_wall_shape.SetAsBox(1.f, 0.01f);
+  VerticalWall left_wall(world, wall_thickness, height/2, height);
+  VerticalWall right_wall(world, width-wall_thickness, height/2, height);
 
-  b2PolygonShape vertical_wall_shape;
-  vertical_wall_shape.SetAsBox(0.01f, 1.f);
+  Bot player_bot(world, 100, height/2);
+  Bot enemy_bot(world, width-100, height/2);
 
-  b2FixtureDef horizontal_wall_fixture;
-  horizontal_wall_fixture.shape = &horizontal_wall_shape;
-
-  b2FixtureDef vertical_wall_fixture;
-  vertical_wall_fixture.shape = &vertical_wall_shape;
-
-  b2BodyDef top_wall_body_def;
-  top_wall_body_def.position.Set(0.f, 0.01f);
-  b2Body* top_wall_body = world.CreateBody(&top_wall_body_def);
-  top_wall_body->CreateFixture(&horizontal_wall_fixture);
-
-  b2BodyDef bottom_wall_body_def;
-  bottom_wall_body_def.position.Set(0.f, 0.99f);
-  b2Body* bottom_wall_body = world.CreateBody(&bottom_wall_body_def);
-  bottom_wall_body->CreateFixture(&horizontal_wall_fixture);
-
-  b2BodyDef left_wall_body_def;
-  left_wall_body_def.position.Set(0.01f, 0.f);
-  b2Body* left_wall_body = world.CreateBody(&left_wall_body_def);
-  left_wall_body->CreateFixture(&vertical_wall_fixture);
-
-  b2BodyDef right_wall_body_def;
-  right_wall_body_def.position.Set(0.99f, 0.f);
-  b2Body* right_wall_body = world.CreateBody(&right_wall_body_def);
-  right_wall_body->CreateFixture(&vertical_wall_fixture);
-
-  // BOTS
-
-  b2BodyDef self_body_def;
-  self_body_def.type = b2_dynamicBody;
-  self_body_def.position.Set(0.1f, 0.5f);
-  self_body_def.linearDamping = 3.f;
-  self_body_def.angularDamping = 3.f;
-
-  b2BodyDef opp_body_def;
-  opp_body_def.type = b2_dynamicBody;
-  opp_body_def.position.Set(0.9f, 0.5f);
-  opp_body_def.linearDamping = 3.f;
-  opp_body_def.angularDamping = 3.f;
-
-  b2Body* self_body = world.CreateBody(&self_body_def);
-  b2Body* opp_body = world.CreateBody(&opp_body_def);
-
-  b2PolygonShape robot_shape;
-  robot_shape.SetAsBox(bot_width_f/2.f, bot_height_f/2.f);
-
-  b2FixtureDef robot_fixture;
-  robot_fixture.shape = &robot_shape;
-  robot_fixture.density = 10.f;
-  robot_fixture.friction = 1.f;
-
-  self_body->CreateFixture(&robot_fixture);
-  opp_body->CreateFixture(&robot_fixture);
-
-  // BALL
+  Ball ball(world);
   
-  b2CircleShape ball_shape;
-  ball_shape.m_radius = 0.02f;
-
-  b2FixtureDef ball_fixture;
-  ball_fixture.shape = &ball_shape;
-  ball_fixture.density = 1.f;
-  ball_fixture.friction = 5.f;
-  
-  b2BodyDef ball_body_def;
-  ball_body_def.type = b2_dynamicBody;
-  ball_body_def.position.Set(0.5f, 0.5f);
-  ball_body_def.linearDamping = 0.5f;
-  ball_body_def.angularDamping = 1.f;
-
-  b2Body* ball_body = world.CreateBody(&ball_body_def);
-  ball_body->CreateFixture(&ball_fixture);
-
-  // PHYSICS
-
   const float timeStep = 1.f / 60.f;
 
   const int velocityIterations = 6;
   const int positionIterations = 2;
-
-  // GRAPHICS
 
   sf::RenderWindow window(sf::VideoMode(width, height), "RoboAI Window", sf::Style::Titlebar);
 
@@ -236,22 +271,22 @@ int main() {
   while (window.isOpen()) {
     window.clear(sf::Color(0, 0, 0));
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
-      Drive(self_body, 0.8, 0.8);
+      Drive(player_bot.body, 0.8, 0.8);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
-      Drive(self_body, -0.8, -0.8);
+      Drive(player_bot.body, -0.8, -0.8);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
-      Drive(self_body, -0.5, 0.5);
+      Drive(player_bot.body, -0.5, 0.5);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::L)) {
-      Drive(self_body, 0.5, -0.5);
+      Drive(player_bot.body, 0.5, -0.5);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::O)) {
-      self_body->SetLinearVelocity(b2Vec2(0, 0));
-      opp_body->SetLinearVelocity(b2Vec2(0, 0));
-      opp_body->SetAngularVelocity(0);
-      self_body->SetAngularVelocity(0);
+      player_bot.body->SetLinearVelocity(b2Vec2(0, 0));
+      enemy_bot.body->SetLinearVelocity(b2Vec2(0, 0));
+      enemy_bot.body->SetAngularVelocity(0);
+      player_bot.body->SetAngularVelocity(0);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
       window.close();
@@ -259,14 +294,14 @@ int main() {
 
     world.Step(timeStep, velocityIterations, positionIterations);
 
-    b2Vec2 ball_position = ball_body->GetPosition();
-    if (ball_position.x > 0.95f && (ball_position.y > 0.35f && ball_position.y < 0.7f)) {
-      ball_body->SetTransform(b2Vec2(0.5f, 0.5f), 0.f);
+    b2Vec2 ball_position = ball.body->GetPosition();
+    if (ball_position.x > 0.95f && (ball_position.y > 0.2f && ball_position.y < 0.8f)) {
+      ball.reset();
     }
 
     world.DebugDraw();
-    debugDraw.DrawCircle(left_wheel(self_body), 0.005, b2Color(0,0,1), true);
-    debugDraw.DrawCircle(right_wheel(self_body), 0.005, b2Color(0,0,1), true);
+    debugDraw.DrawCircle(left_wheel(player_bot.body), 0.005, b2Color(0,0,1), true);
+    debugDraw.DrawCircle(right_wheel(player_bot.body), 0.005, b2Color(0,0,1), true);
     window.display();
 
     i++;
